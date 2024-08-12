3ef7b5286f27ab28d5050dc1bc4fbdd5104f83d4
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-08-12 11:46:04 +0200

    create layout and add history funktion

--- app/src/main/java/de/jonasheilig/currencyconverter/MainActivity.kt
+++ app/src/main/java/de/jonasheilig/currencyconverter/MainActivity.kt
@@ -14,8 +14,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toCurrencySpinner: Spinner
     private lateinit var convertButton: Button
     private lateinit var resultTextView: TextView
+    private lateinit var historyListView: ListView
 
     private val client = OkHttpClient()
+    private val history = mutableListOf<String>()
+    private lateinit var historyAdapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +29,14 @@ class MainActivity : AppCompatActivity() {
         toCurrencySpinner = findViewById(R.id.toCurrencySpinner)
         convertButton = findViewById(R.id.convertButton)
         resultTextView = findViewById(R.id.resultTextView)
+        historyListView = findViewById(R.id.historyListView)
 
         val currencies = arrayOf("USD", "EUR", "GBP", "JPY", "AUD")
         fromCurrencySpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, currencies)
         toCurrencySpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, currencies)
+
+        historyAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, history)
+        historyListView.adapter = historyAdapter
 
         convertButton.setOnClickListener {
             val amount = amountEditText.text.toString().toDoubleOrNull()
@@ -44,7 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchConversionRate(fromCurrency: String, toCurrency: String, amount: Double) {
-        val url = "" // Enter URL
+        val url = "https://api.exchangerate-api.com/v4/latest/$fromCurrency" // Verwende hier eine echte API-URL
 
         val request = Request.Builder().url(url).build()
 
@@ -62,11 +69,18 @@ class MainActivity : AppCompatActivity() {
                     val rate = jsonObject.getJSONObject("rates").getDouble(toCurrency)
                     val convertedAmount = amount * rate
 
+                    val resultText = "$amount $fromCurrency = $convertedAmount $toCurrency"
                     runOnUiThread {
-                        resultTextView.text = "$amount $fromCurrency = $convertedAmount $toCurrency"
+                        resultTextView.text = resultText
+                        updateHistory(resultText)
                     }
                 }
             }
         })
+    }
+
+    private fun updateHistory(resultText: String) {
+        history.add(resultText)
+        historyAdapter.notifyDataSetChanged()
     }
 }
